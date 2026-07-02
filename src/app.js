839d34a64b900408879ea84a643678f7cf69b263
Gracy Mehndiratta <gracy@Gracys-MacBook-Air.local>
2026-07-02 20:19:19 +0530

    feat:patch api route  added

--- src/app.js
+++ src/app.js
@@ -28,5 +28,12 @@ app.delete("/notes/:index",(req,res)=>{
         message:"note deleted successfully",
     })
 })
-
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+  notes[index].description = description;
+  res.status(200).json({
+    message: "note updated  successfully",
+  });
+});
 module.exports=app;
