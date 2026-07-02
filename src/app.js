25f32642e835cba6f2dcfa2a8dfb054ebde54687
Gracy Mehndiratta <gracy@Gracys-MacBook-Air.local>
2026-07-02 20:12:15 +0530

    feat:all rest api's usage

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const app = express();
+
 /*notes , description */
 /* POST /notes */
+const app = express();
+app.use(express.json())
 const notes = []
 
 app.post("/notes", (req, res) => {
-    console.log(req.body)
+    notes.push(req.body)
+
+    res.status(201).json({message:"note created successfully"})
 
 
+})
+
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        message:"notes fetched successfully",
+        notes:notes
+    })
+})
+
+app.delete("/notes/:index",(req,res)=>{
+    const index = req.params.index;
+    delete notes[index]
+    res.status(200).json({
+        message:"note deleted successfully",
+    })
 })
 
 module.exports=app;
